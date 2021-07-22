d2f6eb79608c3f20d924e61aefab02fdf56df900
George <george@hotmail.com>
2021-07-22 11:44:45 +0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+function App() {
+  return (
+  <div>
+    <h1>Welcome!</h1>
+    <div className='card'>
+      <h2>This is my own personal portfolio</h2>
+      <div className='actions'>
+      <button className ='btn'>Home</button>
+      <button className ='btn'>Projects</button>
+      <button className ='btn'>Contact</button>
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default App;
